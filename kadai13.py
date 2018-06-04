41b6252a824c0bcde8ee3d425a865150abe958f2
tokunaga <tokunaga@localhost.localdomain>
2018-06-04 17:00:16 +0900

    メソッドの切り分け、ssh接続失敗時の例外処理追加、出力の修正

--- kadai13.py
+++ kadai13.py
@@ -5,6 +5,7 @@ from kadai6 import vm_module
 import time
 import paramiko
 import sys
+from paramiko import SSHException
 
 def main():
 	# loggerフォーマットメソッド
@@ -13,55 +14,78 @@ def main():
 	args = vm_module.set_args_paramiko()
 	# 接続メソッド
 	si = vm_module.si_connect(args.host ,args.username ,args.password)
-
-	vm_list = si.content.viewManager.CreateContainerView(si.content.rootFolder,
-				[vim.VirtualMachine],
-				True)
+	# CreateContainerViewメソッド 
+	vm_list = vm_module.CreateContainerView(si, vim.VirtualMachine)
 
 	logger.info('開始します')
 	logger.info('-------------------------------------------')
 	for vm in vm_list.view:
-		if vm.runtime.powerState == 'poweredOff':
-			vm.PowerOnVM_Task()
-			logger.info(vm.name + 'の電源をONにしました！')
-			while vm.runtime.powerState == 'poweredOff':
-				logger.info('接続中です')
-				time.sleep(5)
-
-		if vm.runtime.powerState == 'poweredOn':
-			i = 0
-			while vm.summary.guest.ipAddress is None and i < 3:
-				logger.info('%sのIPアドレスを取得しています' % vm.name)
-				time.sleep(60)
-				i = i +1
-				if vm.summary.guest.ipAddress is not None:
-					break
-
-		HOST = vm.summary.guest.ipAddress
-		USER = args.ssh_username
-		PSWD = args.ssh_password
-					
-
-		# ipが取れなかったとき
-		if HOST is None:
-			logger.info('IPアドレスを取得できませんでした')
-			logger.info('終了します')
+		# IP取得メソッド
+		get_id(vm,logger)
+		if vm.summary.guest.ipAddress is None:
+			logger.info('%sのIPアドレスの取得失敗' % vm.name)
+			logger.info('')
+			logger.info('%sへのSSH接続をスキップします' % vm.name)
 			logger.info('-------------------------------------------')
 			continue
 		
+		# 値を代入
+		HOST = vm.summary.guest.ipAddress
+		USER = args.ssh_username
+		PSWD = args.ssh_password
+		
+		# SSH接続メソッド
+		result = connect_ssh(HOST,USER,PSWD,'hostname',logger, vm)
+
+
+
+
+
+
+# IP取得メソッド
+def get_id(vm,logger):
+	if vm.runtime.powerState == 'poweredOff':
+		vm.PowerOnVM_Task()
+		logger.info(vm.name + 'の電源をONにしました！')
+		while vm.runtime.powerState == 'poweredOff':
+			logger.info('接続中です')
+			time.sleep(3)
+	
+	if vm.runtime.powerState == 'poweredOn':
+		i = 0
+		while vm.summary.guest.ipAddress is None and i < 3:
+			logger.info('%sのIPアドレスを取得しています' % vm.name)
+			time.sleep(60)
+			i = i +1
+		if vm.summary.guest.ipAddress is not None:
+			logger.info('%sのIPアドレスの取得成功' % vm.name)
+			logger.info('')
+	
+
+# ssh接続メソッド
+def connect_ssh(HOST,USER,PSWD,command,logger,vm):
+	try:
+		logger.info('SSH接続します...')
 		ssh = paramiko.SSHClient()
 		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		ssh.connect(HOST, username=USER, password=PSWD)
-		stdin, stdout, stderr = ssh.exec_command('hostname')
-		
+		stdin, stdout, stderr = ssh.exec_command(command)
+	except SSHException as e:
+		logger.info(e)
+		logger.info('接続に失敗しました')
+		logger.info('%sへのSSH接続でエラーが発生したのでスキップします' % vm.name)
+		logger.info('-------------------------------------------')
+	# 例外処理が起きなかったら出力
+	else:
+		logger.info('接続に成功しました')
 		if vm.summary.guest.ipAddress is not None:
-			logger.info('IPアドレスが取得できました')
-			logger.info('')
-			logger.info('ssh接続します')
 			for line in stdout:
 				logger.info('{:<15}'.format(vm.name) + line.strip('\n'))
 				logger.info('-------------------------------------------')
 				ssh.close()
+
+
+
 			
 if __name__ == '__main__':
 	main()
